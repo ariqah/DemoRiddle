bec2d418aa9a947bca6c923a2bf6dfde00fb6b1f
ariqah <20023243@rp.edu.sg>
2021-05-19 23:24:53 +0800

    2

--- app/src/main/java/sg/edu/rp/c346/id20023243/demoriddle/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20023243/demoriddle/MainActivity.java
@@ -10,23 +10,40 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvQ1;
-    Button btnRevealQ1;
+    TextView tvQ1, tvQ2;
+    Button btnRevealQ1, btnRevealQ2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         tvQ1 = findViewById(R.id.textViewQ1);
         btnRevealQ1 = findViewById(R.id.buttonRevealQ1);
-
         btnRevealQ1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AnswerActivity1.class);
+                intent.putExtra("Question", "Q1");
+                startActivity(intent);
+            }
+        });
+
+        tvQ2 = findViewById(R.id.textViewQ2);
+        btnRevealQ2 = findViewById(R.id.buttonRevealQ2);
+        btnRevealQ2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AnswerActivity2.class);
+                //change to AnswerActivity1 if want do if statements to check value
+                intent.putExtra("Question", "Q2");
                 startActivity(intent);
             }
         });
     }
-}
+
+
+    }
